5d0103d5f6904819dd73d7902a223d00540f1fc7
PhilipVJ <44639834+PhilipVJ@users.noreply.github.com>
2020-03-11 10:16:14 +0100

    Second commit

---
+++ src/app/portfolio/projectsview/projectsview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projectsview',
+  templateUrl: './projectsview.component.html',
+  styleUrls: ['./projectsview.component.css']
+})
+export class ProjectsviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
