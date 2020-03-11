bcaaf47a72b3102e9dd9c258ffcbdba4443447a7
PhilipVJ <44639834+PhilipVJ@users.noreply.github.com>
2020-03-11 10:07:02 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsviewComponent } from './portfolio/projectsview/projectsview.component';
+import { SocialmediaComponent } from './shared/socialmedia/socialmedia.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import {CarouselModule} from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    AboutComponent,
+    ProjectsviewComponent,
+    SocialmediaComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
