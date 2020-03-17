dc29241d2dfc398606351d85ab8b222d8b7b8d9c
PhilipV <44639834+PhilipVJ@users.noreply.github.com>
2020-03-17 11:12:40 +0100

    webCrawlerAdded

--- src/app/shared/services/projects.service.ts
+++ src/app/shared/services/projects.service.ts
@@ -18,7 +18,7 @@ export class ProjectsService {
       // tslint:disable-next-line:max-line-length
       detailedDescription: 'When Hell Arrived is the first big project I made using the Unity Engine. You control the character John and must fight off monsters near his home in the woods to continue his quest to find his wife.' +
         // tslint:disable-next-line:max-line-length
-        ' In the beginning of the game you can hear, and see a plane in the sky, which is controlled using the new DOTS technology in the engine',
+        ' In the beginning of the game you can hear, and see a plane in the sky, which is controlled using the new DOTS technology in the engine.',
       smallDescription: 'First person shooter where you must fight off monsters in order to survive',
       img: 'assets/images/projects/WhenHellArrived.png',
       techniques: ['Animations, DOTS, Navigation, Physics'],
@@ -39,9 +39,23 @@ export class ProjectsService {
       tools: ['Unity, Visual Studio'],
       downloadLink: 'https://www.dropbox.com/s/2ccdhy40jbhlo21/Zondi.rar?dl=0',
       isSchoolProject: true
-  };
+    };
+    const webCrawler: Project = {
+      category: 'Windows Forms',
+      // tslint:disable-next-line:max-line-length
+      detailedDescription: 'Building an efficient web crawler, which behaves politely on the Internet isn\'t a straight forward task and' +
+        // tslint:disable-next-line:max-line-length
+        ' took a bit longer than I first anticipated. This piece of software allows the user to scrape the web for a searchword, at a given depth, with x amount of agents.',
+      smallDescription: 'Windows application with several options for crawling the web',
+      img: 'assets/images/projects/WebCrawler.png',
+      techniques: ['Multithreading, Autonomous Agents'],
+      title: 'WebCrawler',
+      tools: ['Visual Studio'],
+      githubLink: 'https://github.com/PhilipVJ/WebCrawler',
+      isSchoolProject: true
+    };
 
-    const projectList: Project[] = [whenHellArrived, zondi];
+    const projectList: Project[] = [whenHellArrived, zondi, webCrawler];
     this.allProjects = projectList;
   }
 
